eaf3d72cb97da7e61f9f2c4f209a3114a74d446a
BADUSHKBABU <2badushbabu2@gmail.com>
2023-11-29 19:12:48 +0530

    page for panchayath just created

---
+++ lib/SECONFPAGE/PANCHAYATH/panchayath.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class panchayathscreen extends StatelessWidget {
+  const panchayathscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("PANCHAYATH INFORMATIONS"),),
+    );
+  }
+}
