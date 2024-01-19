3c4df3e139caf75551941781a54815a6b4fe90dc
BADUSHKBABU <2badushbabu2@gmail.com>
2024-01-19 16:42:02 +0530

    added redirection in mesage ,call

--- lib/MODEL/RESTAURANT/restaurant.dart
+++ lib/MODEL/RESTAURANT/restaurant.dart
@@ -1,7 +1,9 @@
 import 'package:clay_containers/clay_containers.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:statemanagement/CONTACT%20%20US/contactus.dart';
 import 'package:statemanagement/MODEL/RESTAURANT/add_restaurant.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
 class restaurant extends StatefulWidget {
@@ -9,8 +11,12 @@ class restaurant extends StatefulWidget {
 
   @override
   State<restaurant> createState() => _restaurantState();
-}
 
+
+
+
+}
+String mobilenumberOfRestaurant="";
 class _restaurantState extends State<restaurant> {
   final CollectionReference hotel= FirebaseFirestore.instance.collection("restaurant");
   @override
@@ -39,6 +45,7 @@ class _restaurantState extends State<restaurant> {
     itemCount:snapshot.data!.docs.length,
     itemBuilder: (context,index){
     final DocumentSnapshot documentSnapshot=snapshot.data!.docs[index];
+    mobilenumberOfRestaurant=documentSnapshot["mobile"];
 
     return ClayContainer(
     width: MediaQuery.of(context).size.width/6,
@@ -53,9 +60,9 @@ class _restaurantState extends State<restaurant> {
     Center(child: Text(documentSnapshot["place"],style: TextStyle(fontSize: 25,color: Colors.black,fontWeight: FontWeight.bold),)),
     Center(child: Row(mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        IconButton(onPressed: (){}, icon: Icon(Icons.phone)),
+        IconButton(onPressed: (){CALL();}, icon: Icon(Icons.phone)),
         IconButton(onPressed: (){}, icon: Icon(Icons.location_on_rounded)),
-        IconButton(onPressed: (){}, icon: Icon(Icons.message)),
+        IconButton(onPressed: (){MESSAGE();}, icon: Icon(Icons.message)),
       ],
     )),
     Divider(color: Colors.white,thickness: 10,)
@@ -66,6 +73,9 @@ class _restaurantState extends State<restaurant> {
 
     ],
     ),
+
+
+
     ),
 
     ///         BUTTON FOR ADDING RESTAURANT
@@ -79,6 +89,25 @@ class _restaurantState extends State<restaurant> {
     )
     );
   }
+MESSAGE() async
+{
+  String mobile="whatsapp://send?phone=$mobilenumberOfRestaurant";
+  try{
+    await launch(mobile);
+  }
+  on FirebaseException catch(e){
+    print("error in launching whatsapp ");
+  }
+}
+
+CALL() async{
+    String calltelephone="tel:$mobilenumberOfRestaurant";
+    try{
+      await launch(calltelephone);
+    }
+    on FirebaseException catch(e){print("cant call this number");
+    }
+}
 
 
 }
